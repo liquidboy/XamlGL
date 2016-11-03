8d6903ea5e59e4b73a85b012d35ff1f50146b8ce
Jose <fajardo_jf@hotmail.com>
2016-11-03 20:54:41 +1100

    TextBoxRenderer : fix the border on lost focus

--- src/XamlGLRenderer/src/XamlGL/Jupiter/Platform/WebGL/Controls/TextBoxRenderer.ts
+++ src/XamlGLRenderer/src/XamlGL/Jupiter/Platform/WebGL/Controls/TextBoxRenderer.ts
@@ -45,7 +45,8 @@ export class TextBoxRenderer extends BaseRenderer implements IControlRenderer {
                 this._cursor.alpha = 0;
 
                 this._borderbackground.clear();
-                this._borderbackground.lineStyle(this._textBoxEl.BorderThickness.Left, RendererHelper.HashToColorNumber(this._textBoxEl.BorderBrush), 1);
+                this._borderbackground.lineStyle(this._textBoxEl.BorderThickness.Left,
+                    RendererHelper.HashToColorNumber(this._textBoxEl.BorderBrush), 1);
                 this._borderbackground.beginFill(RendererHelper.HashToColorNumber(this._textBoxEl.Background), 0.5);
                 this._borderbackground.drawRect(0, 0, this._textBoxEl.CalculatedWidth, this._textBoxEl.CalculatedHeight);
                 this._borderbackground.endFill();
@@ -116,7 +117,8 @@ export class TextBoxRenderer extends BaseRenderer implements IControlRenderer {
         this._borderbackground = new PIXI.Graphics();
         this._borderbackground.width = this._textBoxEl.CalculatedWidth;
         this._borderbackground.height = this._textBoxEl.CalculatedHeight;
-        this._borderbackground.lineStyle( this._textBoxEl.BorderThickness.Left, RendererHelper.HashToColorNumber( this._textBoxEl.BorderBrush), 1);
+        this._borderbackground.lineStyle(this._textBoxEl.BorderThickness.Left,
+            RendererHelper.HashToColorNumber(this._textBoxEl.BorderBrush), 1);
         this._borderbackground.beginFill(RendererHelper.HashToColorNumber(this._textBoxEl.Background), 0.5);
         this._borderbackground.drawRect(0, 0, this._textBoxEl.CalculatedWidth, this._textBoxEl.CalculatedHeight);
         this._borderbackground.endFill();
