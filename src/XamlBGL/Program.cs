d414ad0af6d3a13211fbeb2900f4963b25be8b5a
Jose.Fajardo <fajardo_jf@hotmail.com>
2019-07-01 20:40:24 +1000

    XamlGL : started adding the wasm version of the parser

---
+++ src/XamlBGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XamlBGL
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Hello xaml babylonjs wasm!");
+
+            Console.WriteLine($"Done");
+        }
+    }
+}
